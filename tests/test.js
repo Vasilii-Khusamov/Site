add8df72c231710bc318e09498c42183443d1ddc
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 16:05:02 +0300

    #4 Переформатирован код

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,22 @@
-import { testCalculateDistanceBetweenPoints } from "./testCalculateDistanceBetweenPoints.js"
-import { testPointConstructor } from './testPointConstructor.js'
-import { testVectorConstructor } from './testVectorConstructor.js'
-
-
-
+import {testCalculateDistanceBetweenPoints} from './testCalculateDistanceBetweenPoints.js';
+import {testPointConstructor} from './testPointConstructor.js';
+import {testSubtractVectors} from './testSubtractVectors';
+import {testVectorConstructor} from './testVectorConstructor.js';
 
 const tests = [
-    testCalculateDistanceBetweenPoints,
-    testPointConstructor,
-    testVectorConstructor
+	testCalculateDistanceBetweenPoints,
+	testPointConstructor,
+	testVectorConstructor,
 	testSubtractVectors
+];
 
 for (const test of tests) {
-    try {
-        const start = Date.now()
-        const success = test()
-        const delta = Date.now() - start
-        console.log(test.name, 'OK', `${delta} мс`)
-    } catch (error) {
-        console.log(test.name, error.message)
-    }
+	try {
+		const start = Date.now();
+		const success = test();
+		const delta = Date.now() - start;
+		console.log(test.name, 'OK', `${delta} мс`);
+	} catch (error) {
+		console.log(test.name, error.message);
+	}
 }
