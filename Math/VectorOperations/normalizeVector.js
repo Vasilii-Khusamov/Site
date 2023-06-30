5a761247b0c97bf324cabeb181e1f8e29a6cf7ab
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 17:25:34 +0300

    #8 Создана функция нормализации вектора

---
+++ Math/VectorOperations/normalizeVector.js
@@ -0,0 +1,7 @@
+import {Vector} from '../Vector.js';
+import {calculateVectorLength} from './calculateVectorLength.js';
+
+export function normalizeVector(vector){
+	const vectorLength = calculateVectorLength(vector)
+	return new Vector(vector.x / vectorLength, vector.y / vectorLength)
+}
