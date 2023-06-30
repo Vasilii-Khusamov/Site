8d1becf24bd13afa307aa579a505469990a84b95
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 17:09:41 +0300

    #6 Добавлена тестовая функция

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 import {testCalculateDistanceBetweenPoints} from './testCalculateDistanceBetweenPoints.js';
 import {testCalculateVectorAngle} from './testCalculateVectorAngle.js';
 import {testCalculateVectorAngleByAxisX} from './testCalculateVectorAngleByAxisX.js';
+import {testCalculateVectorLength} from './testCalculateVectorLength.js';
 import {testPointConstructor} from './testPointConstructor.js';
 import {testSubtractVectors} from './testSubtractVectors.js';
 import {testVectorConstructor} from './testVectorConstructor.js';
@@ -11,7 +12,8 @@ const tests = [
 	testVectorConstructor,
 	testSubtractVectors,
 	testCalculateVectorAngle,
-	testCalculateVectorAngleByAxisX
+	testCalculateVectorAngleByAxisX,
+	testCalculateVectorLength
 ];
 
 for (const test of tests) {
