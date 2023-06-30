4f506ec377007d3c4307e4b9e7fd4936f8a9fa2c
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 15:13:27 +0300

    #4 создана тестовая функция

---
+++ tests/testAddVectors.js
@@ -0,0 +1,9 @@
+import { addVectors, Vector } from '../Math/Vector.js'
+
+export function testAddVectors() {
+    const vectorSum = addVectors(new Vector(1, 2), new Vector(3, 4))
+    if (vectorSum.x !== 4 || vectorSum.y !== 6) {
+        throw new Error(`вектора сложились не правильно ${vectorSum}`)
+    }
+    return true
+}
