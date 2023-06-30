1565d568639c585d75a069951456d10bf47f8c97
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 15:16:34 +0300

    #4 Исправления

--- tests/testAddVectors.js
+++ tests/testAddVectors.js
@@ -1,9 +1,9 @@
-import { addVectors, Vector } from '../Math/Vector.js'
+import { addVectors } from '../Math/VectorOperations/addVectors'
 
 export function testAddVectors() {
     const vectorSum = addVectors(new Vector(1, 2), new Vector(3, 4))
     if (vectorSum.x !== 4 || vectorSum.y !== 6) {
-        throw new Error(`вектора сложились не правильно ${vectorSum}`)
+        throw new Error(`РІРµРєС‚РѕСЂР° СЃР»РѕР¶РёР»РёСЃСЊ РЅРµ РїСЂР°РІРёР»СЊРЅРѕ ${vectorSum}`)
     }
     return true
 }
