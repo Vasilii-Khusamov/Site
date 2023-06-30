c55c825bbff2bea2f79e7ff952283dcfae9fdb4e
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 16:53:49 +0300

    #5 Создана тестовая функция

---
+++ tests/testCalculateVectorAngleByAxisX.js
@@ -0,0 +1,11 @@
+import {calculateVectorAngleByAxisX} from '../Math/VectorOperations/calculateVectorAngleByAxisX.js';
+
+export function testCalculateVectorAngleByAxisX(){
+	const resultAngle = calculateVectorAngleByAxisX(new Vector(1, 0))
+	const expectedAngle = Math.PI / 2
+	const epsilon = 0.000000000001
+
+	if (Math.abs(resultAngle - expectedAngle) > epsilon){
+		throw new Error(`Ождалось угол равным ${expectedAngle}, но оно равно ${resultAngle}`)
+	}
+}
