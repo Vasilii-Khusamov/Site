3fd5524453d86879a75f7d0d190d45d6725fc348
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-06-30 17:33:33 +0300

    #8 Создана тестовая функция

---
+++ tests/testNormalizeVector.js
@@ -0,0 +1,13 @@
+import {Vector} from '../Math/Vector.js';
+import {normalizeVector} from '../Math/VectorOperations/normalizeVector.js';
+
+export function testNormalizeVector(){
+	const resultVector = normalizeVector(new Vector(1, 2))
+	const expectedVector = new Vector(0.4472135954999579, 0.8944271909999159)
+	const epsilon = 0.00000000000001
+
+	if (Math.abs(resultVector.x - expectedVector.x) > epsilon || Math.abs(resultVector.y - expectedVector.y) > epsilon){
+		throw new Error(`Ожидался вектор равный ${expectedVector}, но он равен ${resultVector}`)
+	}
+	return true
+}
