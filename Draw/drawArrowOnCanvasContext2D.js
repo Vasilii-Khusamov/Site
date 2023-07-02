36bbe7d8345a55d84f54088652b75bcd9c5cc9d8
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-02 13:20:00 +0300

    #12 Исправлена функция

--- Draw/drawArrowOnCanvasContext2D.js
+++ Draw/drawArrowOnCanvasContext2D.js
@@ -4,4 +4,5 @@ export function drawArrowOnCanvasContext2D(arrowVertexes, ctx){
 	ctx.lineTo(arrowVertexes[2].x, arrowVertexes[2].y)
 	ctx.moveTo(arrowVertexes[1].x, arrowVertexes[1].y)
 	ctx.lineTo(arrowVertexes[3].x, arrowVertexes[3].y)
+	ctx.stroke()
 }
