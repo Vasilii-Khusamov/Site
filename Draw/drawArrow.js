70608269fd3c97c21a55f5c1432ad430c65a3853
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-03 19:57:03 +0300

    #13 Создана функция отрисовки стрелки

---
+++ Draw/drawArrow.js
@@ -0,0 +1,30 @@
+import {createArrowVertexes} from './createArrowVertexes.js'
+import {createAxisXArrowVertexes} from './createAxisXArrowVertexes.js'
+
+/**
+ * Высота головы стрелки по умолчанию.
+ */
+const headArrowDefaultHeight = 10
+
+/**
+ * Ширина головы стрелки по умолчанию.
+ */
+const headArrowDefaultWidth = 10
+
+/**
+ * @param {Point} point1 Начало стрелки.
+ * @param {Point} point2 Конец стрелки (конец это где рисуется голова стрелки).
+ * @param options
+ * @param {Number} options.headArrowHeight
+ * @param {Number} options.headArrowWidth
+ * @param {Function} options.draw
+ */
+export function drawArrow(point1, point2, options) {
+	const { draw } = options
+	// Создаем стрелку на оси Х.
+	const normalizedArrowVertexes = createAxisXArrowVertexes(point1, point2, options)
+	// Создаем стрелку уже на своем месте.
+	const arrowVertexes = createArrowVertexes(point1, point2, normalizedArrowVertexes)
+	// Рисуем стрелку.
+	draw(arrowVertexes)
+}
