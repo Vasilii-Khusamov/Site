695b8d9846e5b61c5619836936fd11ddd3f1eabc
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-03 19:56:26 +0300

    #13 Вектора заменены на точки

--- Draw/createArrowVertexes.js
+++ Draw/createArrowVertexes.js
@@ -7,8 +7,8 @@ import {calculateVectorAngleByAxisX} from '../Math/VectorOperations/calculateVec
 import {Point} from '../Point.js';
 
 /**
- * @param {Vector} point1
- * @param {Vector} point2
+ * @param {Point} point1
+ * @param {Point} point2
  * @param {Vector[]} normalizedArrowVertexes
  * */
 export function createArrowVertexes(point1, point2, normalizedArrowVertexes){
@@ -16,7 +16,7 @@ export function createArrowVertexes(point1, point2, normalizedArrowVertexes){
 	const arrowPosition = new Vector(point1.x, point1.y)
 	let resultArrowVertexes = []
 	for (const normalizedVertex of normalizedArrowVertexes) {
-		resultArrowVertexes.push(addVectors(new Vector(normalizedVertex.x, normalizedVertex.y), arrowPosition))
+		resultArrowVertexes.push(new Point(normalizedVertex.x + arrowPosition.x, normalizedVertex.y + arrowPosition.y))
 	}
 	resultArrowVertexes = multiplyMatrixByVectorArray(createRotationMatrix(arrowAngle), resultArrowVertexes)
 	return resultArrowVertexes
