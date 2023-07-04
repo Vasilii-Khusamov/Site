910e41f4ef7b7baf677ce85637f2eef306303a9f
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-04 13:34:25 +0300

    #14 Создана функция трансформации точки на холст

---
+++ Draw/transformToScreen.js
@@ -0,0 +1,11 @@
+import {Point} from '../Point.js'
+
+/**
+ * Трансформация точек перед отображением на холсте.
+ * Переворачиваем изображение по вертикали и смещаем вниз на высоту холста.
+ * @param {Point} point
+ * @returns
+ */
+export function transformToScreen(point, canvas) {
+	return new Point(point.x, -point.y + canvas.height)
+}
