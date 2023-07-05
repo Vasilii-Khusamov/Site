a584fea5a484e4a80e9e4c363e255fa2389fc6fa
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-05 14:05:35 +0300

    #2 Создана функция расчета напряженности электростатического поля в точке около заряда в векторной форме

---
+++ Physics/calculateElectrostaticFieldIntensityVector.js
@@ -0,0 +1,39 @@
+import {multiplyMatrixByVectorArray} from '../Math/MatrixOperations/multiplyMatrixByVectorArray.js'
+import {calculateVectorLength} from '../Math/VectorOperations/calculateVectorLength.js'
+import {multiplyVectorByNumber} from '../Math/VectorOperations/multiplyVectorByNumber.js'
+import {normalizeVector} from '../Math/VectorOperations/normalizeVector.js'
+import {subtructVectors} from '../Math/VectorOperations/subtractVectors.js'
+import {calculateElectrostaticFieldIntensity} from './calculateElectrostaticFieldIntensity.js'
+
+/**
+ * Формула вычисления напряженности электростатического поля в точке около заряда в векторной форме.
+ * Формула потроена на основе закона Кулона и опредиления напряженности.
+ * @param {number} electricCharge
+ * @param {import('../Math/Vector.js')} electricChargePosition
+ * @param {import('../Math/Vector.js')} testPointPosition
+ * @returns {import('../Math/Vector.js')}
+ */
+export function calculateElectrostaticFieldIntensityVector(electricCharge, electricChargePosition, testPointPosition) {
+	const distance = calculateVectorLength(
+		subtructVectors(
+			testPointPosition,
+			electricChargePosition
+		)
+	)
+
+	const normalisedIntensityVector = normalizeVector(
+		subtructVectors(
+			testPointPosition, electricChargePosition
+		)
+	)
+
+	return (
+		multiplyVectorByNumber(
+			normalisedIntensityVector,
+			calculateElectrostaticFieldIntensity(
+				distance,
+				electricCharge
+			)
+		)
+	)
+}
