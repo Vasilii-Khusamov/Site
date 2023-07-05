9751dd7831d308e4703a477fcd1fc0bd6e92d989
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-05 13:53:55 +0300

    #2 Создан файл с константами

---
+++ Physics/Constants.js
@@ -0,0 +1,8 @@
+import {calculateElectrostaticFieldIntensity} from './calculateElectrostaticFieldIntensity.js'
+
+/**
+ * Коеффициент для вычисления напряженности электростатического поля в точке около заряда.
+ * Еденица измерения: Н * М^2 / Кл^2
+ * @type {number}
+ */
+export const electrostaticFieldIntensityRatio = 9e9
