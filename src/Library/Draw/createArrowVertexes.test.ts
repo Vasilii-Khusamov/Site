4b55965eb2f1c383c2f80d4dd2ff68582fa61dac
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-27 20:01:22 +0300

    #32 Создан тест

---
+++ src/Library/Draw/createArrowVertexes.test.ts
@@ -0,0 +1,24 @@
+import {Point} from '../Math/Point'
+import {Vector} from '../Math/Vector'
+import {createArrowVertexes} from './createArrowVertexes'
+
+test('createArrowVertexes', () => {
+    const expectedArrowVertexes = [
+        new Point(50, 0),
+        new Point(50, 100),
+        new Point(55, 90),
+        new Point(45, 90),
+    ]
+    const normalizedArrowVertexes = [
+        new Vector(0, 0),
+        new Vector(100, 0),
+        new Vector(90, -5),
+        new Vector(90, 5),
+    ]
+    const resultArrowVertexes = createArrowVertexes(new Point(50, 0), new Point(50, 100), normalizedArrowVertexes)
+    const epsilon = 1e-10
+    for (let i = 0; i < resultArrowVertexes.length; i++) {
+        expect(Math.abs(resultArrowVertexes[i].x - expectedArrowVertexes[i].x)).toBeLessThan(epsilon)
+        expect(Math.abs(resultArrowVertexes[i].y - expectedArrowVertexes[i].y)).toBeLessThan(epsilon)
+    }
+})
