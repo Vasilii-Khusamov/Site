c78816606029f6d018db9bd6be1cb79a7237f07a
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-27 19:48:41 +0300

    #32 Создан тест

---
+++ src/Library/Draw/createAxisXArrowVertexes.test.ts
@@ -0,0 +1,21 @@
+import {createAxisXArrowVertexes} from './createAxisXArrowVertexes'
+import {Point} from '../Math/Point'
+
+test('createAxisXArrowVertexes', () => {
+    const expectedArrowVertexes = [
+        new Point(0, 0),
+        new Point(100, 0),
+        new Point(90, 5),
+        new Point(90, -5),
+    ]
+    const resultArrowVertexes = createAxisXArrowVertexes(
+        new Point(0, 50),
+        new Point(100, 50),
+        {}
+    )
+    expect(resultArrowVertexes.length).toBe(4)
+    for (let i = 0; i < resultArrowVertexes.length; i++) {
+        expect(resultArrowVertexes[i].x).toBe(expectedArrowVertexes[i].x)
+        expect(resultArrowVertexes[i].y).toBe(expectedArrowVertexes[i].y)
+    }
+})
