86981c4575cd1d3af9aec42acd64521944be37f4
Vasilii Khusamov <Vasilii.Khusamov@yandex.ru>
2023-07-28 20:25:42 +0300

    #32 Создан тест

---
+++ src/Library/Math/MatrixOperations/multiplyMatrixByVector.test.ts
@@ -0,0 +1,13 @@
+import {Vector} from "../Vector";
+import {multiplyMatrixByVector} from "./multiplyMatrixByVector";
+
+test('multiplyMatrixByVector', () => {
+    const matrix = [
+        [1, 2],
+        [3, 4]
+    ]
+    const expectedVector = new Vector(8, 18)
+    const resultVector = multiplyMatrixByVector(matrix, new Vector(2, 3))
+
+    expect(resultVector).toEqual(expectedVector)
+})
